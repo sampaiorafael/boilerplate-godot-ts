5595b957c32d21724045deaa64258ac8c35fca34
sampaiorafael <rafa021dev@gmail.com>
2021-04-18 14:01:18 -0300

    feat: project keymapping enum

---
+++ src/utils/keyMapping.ts
@@ -0,0 +1,18 @@
+/**
+ * @godot Project -> Project Settings -> Input Map
+ */
+export enum keyMapping {
+    ENTER = 'ui_accept',
+    SPACE = 'ui_select',
+    BACKSPACE = 'ui_cancel',
+    TAB = 'ui_focus_next',
+    SHIFT_TAB = 'ui_focus_prev',
+    LEFT = 'ui_left',
+    RIGHT = 'ui_right',
+    UP = 'ui_up',
+    DOWN = 'ui_down',
+    PAGE_UP = 'ui_page_up',
+    PAGE_DOWN = 'ui_page_down',
+    HOME = 'ui_home',
+    END = 'ui_end'
+}
