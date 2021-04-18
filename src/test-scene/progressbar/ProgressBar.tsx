1c5b99a665e86c83732b36ba99f8e985155d133c
sampaiorafael <rafa021dev@gmail.com>
2021-04-18 13:54:19 -0300

    doc: progress bar comments

--- src/test-scene/progressbar/ProgressBar.tsx
+++ src/test-scene/progressbar/ProgressBar.tsx
@@ -5,6 +5,11 @@ export default class InputLine extends godot.ProgressBar {
 
     public progressBarVal: number = 0
 
+    /**
+     * if actual progress bar value >= 100, reset this.progresBarval to 0
+     * then increase progress bar by the actual this.progressBarVal number
+     * @return void
+     */
     public increaseProgressBar (): void {
 
         if (this.get_value() >= 100 )
@@ -13,6 +18,10 @@ export default class InputLine extends godot.ProgressBar {
         this.set_value(this.progressBarVal)     
     }
 
+    /**
+     * @param step step value of progressbar progress
+     * @param percentVisible progressbar percent visibility
+     */
     public progressBarConfigs (step: number, percentVisible: boolean): void {
         this.set_percent_visible(percentVisible) // this.percent_visible = true
         this.set_step(step) // this.step = 10
