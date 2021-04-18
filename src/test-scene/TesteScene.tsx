44ebff39ccf9b7e6c8b652807059a2bb88e778e2
sampaiorafael <rafa021dev@gmail.com>
2021-04-18 12:25:39 -0300

    feat: test-scene

---
+++ src/test-scene/TesteScene.tsx
@@ -0,0 +1,17 @@
+import { signal, property, tool, onready, node } from '../../decorators';
+
+@tool // make the script runnable in godot editor
+export default class InputLine extends godot.Script {
+
+    _input (event: any): void {
+
+    }
+    
+	_ready (): void {
+    }
+
+
+    _process (delta): void {
+    }
+
+}
